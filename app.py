96a48effe180de0d9020e156df9a376e99b1f5d1
Lou Dela Cruz <nerilou.delacruz@wvsu.edu.ph>
2023-11-14 12:55:39 +0800

    Update app.py

--- app.py
+++ app.py
@@ -14,32 +14,37 @@ p = 'shape_predictor_68_face_landmarks.dat'
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 
-def video_frame_callback(frame):
-    img = frame.to_ndarray(format="bgr24")
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    rects = detector(gray, 0)
+class VideoProcessor:
+    
+    def recv(self, frame):
+        img = frame.to_ndarray(format="bgr24")
         
-    for (i, rect) in enumerate(rects):
-        s = predictor(gray, rect)
-        s = face_utils.shape_to_np(s)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-        for(i, y) in s:
-            cv2.circle(img, (i,y), 2, (0, 255, 0), -1)
+        rects = detector(gray, 0)
+        
+        for (i, rect) in enumerate(rects):
+        	s = predictor(gray, rect)
+        	s = face_utils.shape_to_np(s)
 
-    # ... Image processing, or whatever you want ...
+        	for(i, y) in s:
+        		cv2.circle(img, (i,y), 2, (0, 255, 0), -1)
 
-    return av.VideoFrame.from_ndarray(img, format="bgr24")
+        return av.VideoFrame.from_ndarray(img, format="bgr24")
 
 
+    
 RTC_CONFIGURATION = RTCConfiguration(
     {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
 )
         
+webrtc_ctx = webrtc_streamer(key="dlib", 
+                              mode=WebRtcMode.SENDRECV,
+                              rtc_configuration=RTC_CONFIGURATION,
+                              video_processor_factory=VideoProcessor,
+                              media_stream_constraints={"video": True, "audio": False},
+                              async_processing=True)
 
-webrtc_ctx = webrtc_streamer(key="example", 
-                mode=WebRtcMode.SENDRECV, 
-                rtc_configuration=RTC_CONFIGURATION, 
-                video_frame_callback=video_frame_callback)
 
 while True:
     if webrtc_ctx.video_transformer:
@@ -47,7 +52,6 @@ while True:
         labels_placeholder.table(result)
     else:
         break
-
 
 
 
