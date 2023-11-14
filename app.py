de019e867bec2646b74c92821adfb208a890bf23
techministerJays <101700771+techministerJays@users.noreply.github.com>
2023-11-14 17:21:13 +0800

    Update app.py

--- app.py
+++ app.py
@@ -37,8 +37,20 @@ class VideoProcessor:
         	s = predictor(gray, rect)
         	s = face_utils.shape_to_np(s)
 
-        	for(i, y) in s:
-        		cv2.circle(img, (i,y), 2, (0, 255, 0), -1)
+        	#for(i, y) in s:
+        		#cv2.circle(img, (i,y), 2, (0, 255, 0), -1)
+        if i==0:
+                print("Total number of face landmarks detected ",len(s_.parts()))
+                
+                drawPoints(img, s_, 0, 16)           # Jaw line
+                drawPoints(img, s_, 17, 21)          # Left eyebrow
+                drawPoints(img, s_, 22, 26)          # Right eyebrow
+                drawPoints(img, s_, 27, 30)          # Nose bridge
+                drawPoints(img, s_, 30, 35, True)    # Lower nose
+                drawPoints(img, s_, 36, 41, True)    # Left eye
+                drawPoints(img, s_, 42, 47, True)    # Right Eye
+                drawPoints(img, s_, 48, 59, True)    # Outer lip
+                drawPoints(img, s_, 60, 67, True)    # Inner lip
 
         return av.VideoFrame.from_ndarray(img, format="bgr24")
 
